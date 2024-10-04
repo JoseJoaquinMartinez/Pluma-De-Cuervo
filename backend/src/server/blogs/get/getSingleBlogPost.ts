9ccad3c71e3762b92d1938a56e6614dfa2494d85
José Joaquín <jj.martinez87@hotmail.com>
2024-10-04 22:33:28 +0200

    get single blog endpoints and tests

--- backend/src/server/blogs/get/getSingleBlogPost.ts
+++ backend/src/server/blogs/get/getSingleBlogPost.ts
@@ -6,10 +6,10 @@ const prisma = new PrismaClient();
 
 router.get("/get-single-blog-post/:blogId", async (req, res) => {
   const blogId = parseInt(req.params.blogId);
+  if (isNaN(blogId)) {
+    return res.status(400).json({ message: "Id del blog no recibido" });
+  }
   try {
-    if (!blogId) {
-      return res.status(400).json({ message: "Id del blog no recibido" });
-    }
     const existingBlog = await prisma.blog.findFirst({ where: { id: blogId } });
     if (!existingBlog) {
       return res.status(404).json({ message: "Blog post no encontrado" });
