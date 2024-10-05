c6b07bf053ba5105653f1d8b378d5cba0d6cf074
José Joaquín <jj.martinez87@hotmail.com>
2024-10-05 12:22:38 +0200

    DELETE deleteBlogPost endpoint and testing completed

---
+++ backend/src/server/blogs/delete/deleteBlogPost.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
+const router = Router();
+const prisma = new PrismaClient();
+
+router.delete("/delete-blog-post/:blogId", async (req, res) => {
+  const blogId = parseInt(req.params.blogId);
+
+  if (isNaN(blogId)) {
+    return res.status(400).json({ message: "Id del blog no recibido" });
+  }
+
+  try {
+    const existingBlog = await prisma.blog.delete({ where: { id: blogId } });
+
+    return res.status(200).json({ message: "Entrada de blog eliminada" });
+  } catch (error) {
+    if (
+      error instanceof PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    ) {
+      return res.status(404).json({ message: "Entrada de blog no encontrada" });
+    } else {
+      return res.status(500).json({
+        error: `Error inesperando eliminando la entrada de blog ${error.message}`,
+      });
+    }
+  } finally {
+    prisma.$disconnect();
+  }
+});
+
+export default router;
