ff1cef6f70f395828664aca01015acf79c2db1d4
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2024-10-24 06:33:42 +0000

    fixing stiling

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
-import Logo from "../Logo";
+import Logo from "../shared/Logo";
 import TiktokLogo from "./tiktokLogo";
 import SecondaryButton from "../shared/SecondaryButton";
 
 export default function Footer() {
   return (
     <div className="bg-navFoot w-full">
-      <section className="max-w-screen-xl flex items-center justify-between h-auto mx-auto">
+      <section className="max-w-screen-xl flex flex-col items-center justify-between h-auto mx-auto">
         <Link
           href="/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
@@ -20,7 +20,7 @@ export default function Footer() {
         >
           <TiktokLogo width={"50"} height={"50"} />
         </Link>
-        <article className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <article className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse ">
           <SecondaryButton link="/contact" name="Contacto" />
         </article>
       </section>
