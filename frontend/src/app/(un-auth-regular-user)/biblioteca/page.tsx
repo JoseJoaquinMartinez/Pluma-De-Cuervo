266915eee2d60f7fd15e57ce60367a890180bf33
José Joaquín <jj.martinez87@hotmail.com>
2024-11-22 21:02:40 +0100

    add slider to library page

--- frontend/src/app/(un-auth-regular-user)/biblioteca/page.tsx
+++ frontend/src/app/(un-auth-regular-user)/biblioteca/page.tsx
@@ -1,10 +1,12 @@
-import {SectionComponent} from "@/homepage/components/SectionComponent";
-import BookCardComponent from "@/app/(un-auth-regular-user)/biblioteca/components/BookCardComponent";
+import { SectionComponent } from "@/homepage/components/SectionComponent";
+import BookCardComponent from "@/books/book/BookCardComponent";
 
 export default function Biblioteca() {
-//TODO add slider to show all the books
+  //TODO add slider to show all the books
 
-  return <div className="flex flex-col items-center justify-center pb-8 px-4">
-    <SectionComponent title="Biblioteca" content={<BookCardComponent/>}/>
-  </div>
+  return (
+    <div className="flex flex-col items-center justify-center pb-7 md:pb-0 px-4">
+      <SectionComponent title="Biblioteca" content={<BookCardComponent />} />
+    </div>
+  );
 }
---
+++ frontend/src/components/shared/Slider.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import React, { useState } from "react";
+import BookCard from "@/books/components/BookCard";
+import Image from "next/image";
+
+const Slider = ({ books }: { books: BookCardComponentProps[] }) => {
+  const [activeBook, setActiveBook] = useState<number>(0);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const clickNext = () => {
+    setActiveBook((prev) => (prev === books.length - 1 ? 0 : prev + 1));
+    scrollToTop();
+  };
+
+  const clickPrev = () => {
+    setActiveBook((prev) => (prev === 0 ? books.length - 1 : prev - 1));
+    scrollToTop();
+  };
+
+  return (
+    <section className="flex flex-col items-center w-full">
+      <article className="w-full flex justify-center items-center overflow-hidden transition-transform ease-in-out duration-500">
+        <div
+          className="w-full flex transition-transform duration-500 ease-in-out"
+          style={{
+            transform: `translateX(-${activeBook * 100}%)`,
+          }}
+        >
+          {books.map(
+            (
+              { id, title, Synopsis, imagen }: BookCardComponentProps,
+              index,
+            ) => (
+              <div key={id} className="w-full flex-shrink-0">
+                <BookCard
+                  id={id}
+                  title={title}
+                  Synopsis={Synopsis}
+                  imagen={imagen}
+                />
+              </div>
+            ),
+          )}
+        </div>
+      </article>
+
+      <article className="flex justify-between items-center gap-5 mt-2">
+        <button
+          onClick={clickPrev}
+          className="p-2 bg-cardsBackground rounded-full shadow hover:bg-cardsBackground/50"
+        >
+          <Image
+            alt="arrow left"
+            src="/shared/arrow-left.png"
+            width={30}
+            height={30}
+          />
+        </button>
+        <button
+          onClick={clickNext}
+          className="p-2 bg-cardsBackground rounded-full shadow hover:bg-cardsBackground/50"
+        >
+          <Image
+            alt="arrow right"
+            src="/shared/arrow-right.png"
+            width={30}
+            height={30}
+          />
+        </button>
+      </article>
+    </section>
+  );
+};
+
+export default Slider;
