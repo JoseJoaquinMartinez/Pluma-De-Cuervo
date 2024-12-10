8d366e703d3c62edefd77974823a0ee4a774d79a
José Joaquín Martínez Carrillo <josejoaquin@MacBook-Air-de-Jose.local>
2024-12-10 14:37:17 +0100

    getSingleChapterSlice completed

--- frontend/src/store/slices/chapter/singleChapterSlice.ts
+++ frontend/src/store/slices/chapter/singleChapterSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchSingleChapter } from "./thunks/fetchSingleChapter";
 import type { ChapterProps } from "@/chapters/chapter/interface/chapter";
+import { stat } from "fs";
 
 const initialState: {
     data: ChapterProps | null;
@@ -18,6 +19,16 @@ const getSingleChapterSlice = createSlice({
     reducers: {},
     extraReducers: (builder) =>{
         builder
-        .addCase(fetch)
+        .addCase(fetchSingleChapter.fulfilled, (state, action) =>{
+            state.loading = false;
+            state.data = action.payload
+        })
+        .addCase(fetchSingleChapter.pending, (state) =>{
+            state.loading = true;
+        })
+        .addCase(fetchSingleChapter.rejected, (state, action) =>{
+            state.loading = false;
+            state.error = action.payload as string;
+        })
     }
 })
