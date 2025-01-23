dbbb7a11a5636b5faa74994911e766068d77b225
José Joaquín <jj.martinez87@hotmail.com>
2025-01-23 21:24:31 +0100

    feat: implementar formulario para crear nuevas entradas de blog con soporte para carga de imágenes

--- frontend/src/blogs/components/AllBlogsComponent.tsx
+++ frontend/src/blogs/components/AllBlogsComponent.tsx
@@ -5,6 +5,7 @@ import { AppDispatch, RootState } from "@/store/store";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BlogCardComponent } from "./BlogCardComponent";
+import MainButton from "@/components/shared/mainButton";
 
 function AllBlogsComponent() {
   const dispatch = useDispatch<AppDispatch>();
@@ -13,6 +14,7 @@ function AllBlogsComponent() {
     loading,
     error,
   } = useSelector((state: RootState) => state.AllBlogs);
+  const { data } = useSelector((state: RootState) => state.Authentication);
 
   useEffect(() => {
     if (!blogs || blogs?.length === 0) {
@@ -29,9 +31,16 @@ function AllBlogsComponent() {
   if (blogs) {
     return (
       <section className="flex flex-col items-center mt-5 lg:max-w-screen-xl ">
-        <h2 className="text-encabezados text-2xl  text-start mb-2 self-start px-2 font-semibold">
-          Noticias & Pensamientos
-        </h2>
+        <div className="flex flex-col md:flex-row mb-2 w-full justify-between">
+          <h2 className="text-encabezados text-2xl  text-start mb-2 self-start px-2 font-semibold">
+            Noticias & Pensamientos
+          </h2>
+          <article className="self-center">
+            {data?.user.role === "admin" && (
+              <MainButton name="Nuevo Blog" link="/admin/blogs/crear-blog" />
+            )}
+          </article>
+        </div>
         {blogs && blogs.length === 0 && <p>No hay blogs que mostrar</p>}
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {blogs.map(({ id, title, createdAt, estimatedReadTime, imagen }) => (
