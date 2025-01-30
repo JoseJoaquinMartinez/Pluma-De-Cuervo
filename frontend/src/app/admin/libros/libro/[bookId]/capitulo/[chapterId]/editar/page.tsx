e901178c82e58b69f59c0a4eb3f108b46d216dff
José Joaquín <jj.martinez87@hotmail.com>
2025-01-30 20:34:56 +0100

    feat: agregar funcionalidad para editar capítulos y mostrar botones de edición para administradores

---
+++ frontend/src/app/admin/libros/libro/[bookId]/capitulo/[chapterId]/editar/page.tsx
@@ -0,0 +1,18 @@
+import { SectionComponent } from "@/homepage/components/SectionComponent";
+import { EditChapter } from "../../../../../../../../chapters/chapter/editChapter";
+
+export default function EditChapterPage({
+  params,
+}: {
+  params: { chapterId: string };
+}) {
+  const chapterId = parseInt(params.chapterId);
+  return (
+    <div className="flex flex-col items-center justify-center  pb-8 px-4">
+      <SectionComponent
+        title="Edita el capítulo"
+        content={<EditChapter chapterId={chapterId} />}
+      />
+    </div>
+  );
+}
---
+++ frontend/src/chapters/chapter/editChapter.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const EditChapter = ({ chapterId }: { chapterId: number }) => {
+  return <div>EditChapter</div>;
+};
