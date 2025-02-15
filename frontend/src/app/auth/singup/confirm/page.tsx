4c349426d02f327f81b0b7c8585e0e130635df16
José Joaquín <jj.martinez87@hotmail.com>
2025-02-15 21:17:15 +0100

    feat: mejorar la página de confirmación de registro con manejo de errores y carga asíncrona

--- frontend/src/app/auth/singup/confirm/page.tsx
+++ frontend/src/app/auth/singup/confirm/page.tsx
@@ -1,17 +1,32 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import RegistrationComponent from "@/app/authComponents/singup/RegistrationComponent";
 import { useSearchParams } from "next/navigation";
+import { BookLoaderComponent } from "@/components/shared/BookLoader";
 
 export default function SingUpConfirmationPage() {
   const params = useSearchParams();
-  const token = params.get("token");
+  const token = params?.get("token");
 
-  if (token) {
+  if (!token) {
     return (
-      <section className="flex flex-col items-center justify-center ">
-        <RegistrationComponent token={token} />
-      </section>
+      <p className="text-mainText text-xl">
+        Token no encontrado. Por favor, verifica tu enlace.
+      </p>
     );
   }
+
+  return (
+    <Suspense
+      fallback={
+        <div className="flex flex-col items-center justify-center w-full">
+          <BookLoaderComponent />
+        </div>
+      }
+    >
+      <section className="flex flex-col items-center justify-center">
+        <RegistrationComponent token={token} />
+      </section>
+    </Suspense>
+  );
 }
