1ccf99e9d022fdec019315eeb496f369314ec29c
José Joaquín <jj.martinez87@hotmail.com>
2025-02-15 23:33:35 +0100

    feat: actualizar metadatos y optimizar la configuración de Open Graph y Twitter en layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -8,13 +8,51 @@ import CookieBanner from "@/components/shared/CookieBanner";
 
 const onest = localFont({
   src: "../../public/fonts/Onest-VariableFont_wght.ttf",
-  variable: "--font-önest",
+  variable: "--font-onest",
 });
 
 export const metadata: Metadata = {
-  title: "Pluma de cuervo",
+  title: "Pluma de Cuervo | Obras de Gabriel P.",
   description:
-    "Pluma de Cuervo el lugar perfecto para disfrutar de las obras de Gabriel P.",
+    "Pluma de Cuervo es el lugar perfecto para disfrutar de las obras literarias de Gabriel P.",
+  openGraph: {
+    title: "Pluma de Cuervo",
+    description:
+      "Explora las obras literarias de Gabriel P. en Pluma de Cuervo.",
+    url: "https://pluma-de-cuervo-h5js.vercel.app/",
+    siteName: "Pluma de Cuervo",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dk9juz4fp/image/upload/v1739611427/Pluma%20de%20Cuervo/igvlg1cr6ntol97a2ct5.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Portada de Pluma de Cuervo",
+      },
+    ],
+    locale: "es_ES",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Pluma de Cuervo",
+    description:
+      "Explora las obras literarias de Gabriel P. en Pluma de Cuervo.",
+    images: [
+      "https://res.cloudinary.com/dk9juz4fp/image/upload/v1739611427/Pluma%20de%20Cuervo/igvlg1cr6ntol97a2ct5.jpg",
+    ],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  keywords: [
+    "Pluma de Cuervo",
+    "Gabriel P.",
+    "Literatura",
+    "Escritor",
+    "Obras literarias",
+    "Libros",
+  ],
 };
 
 export default function RootLayout({
@@ -29,9 +67,7 @@ export default function RootLayout({
       >
         <Providers>
           <Navbar />
-
           <main className="flex-grow">{children}</main>
-
           <Footer />
           <CookieBanner />
         </Providers>
