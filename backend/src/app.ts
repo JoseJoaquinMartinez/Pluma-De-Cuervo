847445fd19da4b0635a0ab2c956cc8099e37d62f
José Joaquín <jj.martinez87@hotmail.com>
2025-02-15 22:46:59 +0100

    feat: restaurar configuración de CORS y agregar encabezados específicos para permitir solicitudes desde un origen definido

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -70,8 +70,7 @@ const corsOptions = {
   credentials: true,
 };
 
-/* app.use(cors(corsOptions)); */
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Backend en funcionamiento");
