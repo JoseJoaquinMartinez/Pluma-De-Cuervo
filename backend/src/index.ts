44b447657a78bee1ab16b18c68dee2adeb857646
José Joaquín <jj.martinez87@hotmail.com>
2025-02-16 00:30:48 +0100

    feat: agregar configuración para iniciar el servidor en el puerto 3000

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -78,6 +78,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port 3000");
+});
+
 //USER
 app.use("/user", changeUserName);
 
