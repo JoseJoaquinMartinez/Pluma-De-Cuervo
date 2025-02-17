184c2c5a0287db8f01ada8f52cf3a42e0695a87e
José Joaquín <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2025-02-17 13:15:09 +0000

    fix: corregir la sintaxis del componente de inicio de sesión eliminando el punto y coma innecesario

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginComponent } from "@/app/authComponents/login/LoginComponent";
 export default function LoginPage() {
   return (
     <section className=" px-2 md:px-0 mt-10 self-center w-full ">
-      <LoginComponent />;
+      <LoginComponent />
     </section>
   );
 }
