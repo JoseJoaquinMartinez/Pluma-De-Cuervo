d804def549bf6db880bbec1f6eaeff2d1343b82a
José Joaquín <jj.martinez87@hotmail.com>
2025-07-06 11:45:21 +0200

    fix: correct link path for "Otros Trabajos" in admin navigation items

--- frontend/src/components/navbar/data/nav-items.ts
+++ frontend/src/components/navbar/data/nav-items.ts
@@ -30,7 +30,7 @@ export const adminNavItems = [
     name: "Comentarios",
   },
   { link: "/admin/contacto", name: "Contacto" },
-  { link: "admin/otros-trabajos", name: "Otros Trabajos" },
+  { link: "/admin/otros-trabajos", name: "Otros Trabajos" },
 ];
 export const regularNavItems = [
   {
